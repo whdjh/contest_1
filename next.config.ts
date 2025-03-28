f37d57981d9c4d9189b2bac0a187ecca5446369c
whdjh <whdjh98@naver.com>
2025-03-28 16:35:26 +0900

    feat: svg 로더 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/, // SVG 파일을 찾습니다.
+      use: [
+        {
+          loader: '@svgr/webpack', // SVGR 로더 사용
+          options: {
+            icon: true, // 기본적으로 SVG를 아이콘 크기로 조정
+          },
+        },
+      ],
+    });
+    return config;
+  },
+
+  trailingSlash: true,  // URL 끝에 슬래시(/)를 자동으로 추가
+  images: {
+    domains: ['sprint-fe-project.s3.ap-northeast-2.amazonaws.com'], // 외부 이미지 도메인 추가
+    unoptimized: true, // 이미지 최적화 비활성화
+  },
 };
 
 export default nextConfig;
