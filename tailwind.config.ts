1b2106f58e4062a5edea064ac7a19b0347b00e31
whdjh <whdjh98@naver.com>
2025-03-28 16:21:24 +0900

    feat: color 적용

---
+++ tailwind.config.ts
@@ -0,0 +1,30 @@
+import type {Config} from 'tailwindcss';
+
+export default {
+  darkMode: 'class',
+  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      colors: {
+        white: '#ffffff',
+        black: '#1b1b1b',
+        gray: '#79747E',
+        red: '#FF472E',
+        orange: '#FF7C1D',
+        yellow: '#ffc23d',
+        green: '#00ac07',
+        blue: '#0085FF',
+        navy: '#000080',
+        purple: '#A020F0',
+      },
+      screens: {
+        // PC (1200px 이상)
+        pc: '1200px',
+        // Tablet (745px 이상 ~ 1199px 이하)
+        tablet: '745px',
+        // Mobile (375px 이상 ~ 744px 이하)
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
