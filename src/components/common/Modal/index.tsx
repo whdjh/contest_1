ae301daf4ecc32622a9f0de5dabc6f6fbd2801de
jaehyeongjung <titochung625@naver.com>
2025-04-04 16:23:58 +0900

    refactor: modal 타입파일 따로 관리기

--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -1,12 +1,8 @@
 'use client';
 
 import React from 'react';
+import { ModalProps } from '@/types/modal';
 
-interface ModalProps {
-  isOpen: boolean;
-  onClose: () => void;
-  children: React.ReactNode;
-}
 
 export default function Modal({ isOpen, onClose, children }: ModalProps) {
   if (!isOpen) return null;
@@ -21,10 +17,9 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
           onClick={onClose}
           className="absolute top-3 right-3 text-gray-500 hover:text-black"
         >
-          ✖ {/* 이미지로 교체해도 좋나, 그대로 써도 무방하긴 할듯? */}
+          ✖ 
         </button>
-        {children}{' '}
-        {/* 자식으로 받아서 뭘 띄울지, 경고창이 됐든, 금지언어가 포함됐다고 띄우든.. */}
+        {children}
       </div>
     </div>
   );
