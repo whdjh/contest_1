89813abe4bf06882ce0cfa642a8097a998bc5309
jaehyeongjung <titochung625@naver.com>
2025-04-06 16:26:48 +0900

    chore: mainPage의 layout 셋팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
   return (
-    <div>
-      메인페이지 작업 시작 커밋
-      
-    </div>
+    <main>
+      <section></section>
+      <section></section>
+      <section></section>
+    </main>
   );
 }
