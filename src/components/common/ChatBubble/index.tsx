9aeef6e309271da576ad5932e5e896fc366d96ea
whdjh <whdjh98@naver.com>
2025-04-06 16:33:18 +0900

    feat: timestamp 자리 추가

--- src/components/common/ChatBubble/index.tsx
+++ src/components/common/ChatBubble/index.tsx
@@ -15,11 +15,12 @@ export default function ChatBubble({
           <div className={`w-full flex ${alignRight ? 'justify-end' : 'justify-start'}`}>
             <div
               className={`
-                whitespace-pre-wrap break-words px-3 py-1 rounded-md text-black max-w-[70%]
+                whitespace-pre-wrap break-words px-3 py-1 rounded-lg text-black max-w-[70%]
                 ${userStatus ? 'bg-amber-300' : 'bg-white'}
               `}
             >
               {value}
+              <h3 className='flex justify-end text-[8px]'>01:30</h3>
             </div>
           </div>
         );
