29db3b8d4392f5ca2b097120b267234e99a74dc4
jaehyeongjung <titochung625@naver.com>
2025-04-11 17:36:30 +0900

    chore: 브랜치명 하이픈 오류

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import HeroSection from '@/components/product/main/TextSeciton';
-import MainSection from '@/components/product/main/ImageSection';
+import TextSection from '@/components/product/main/TextSeciton';
+import ImageSection from '@/components/product/main/ImageSection';
 import LinkSection from '@/components/product/main/LinkSection';
 
 export default function Home() {
   return (
     <>
       <main className="bg-black">
-          <HeroSection key="hero"/>
-          <MainSection key="main"/>
+          <TextSection key="hero"/>
+          <ImageSection key="main"/>
           <LinkSection key="link"/>
       </main>
     </>
